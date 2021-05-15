08ccf577e0ee3839bb7b8b88e46b259d72fbc19d
SATYAM MANDAL <satyam.mandal.77@gmail.com>
2021-05-15 16:04:30 +0530

    add Interface of GoodReceipt mapping and Goodreceipt

---
+++ ServerSide/ClassLibrary1/Interfaces/IGoodReceipt.cs
@@ -0,0 +1,22 @@
+﻿using Shopping.Common.Requests;
+using Shopping.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.BLayer.Interfaces
+{
+    interface IGoodReceipt
+    {
+        Task<GoodReceiptResponse> savedetails(GoodReceiptRequest data, string filter);
+        Task<GoodReceiptResponse> updatedetails(GoodReceiptRequest data, string filter);
+        Task<GoodReceiptResponse> getdetailbyGrnumber(int grnum, string filter);
+        Task<List<GoodReceiptResponse>> getdetailsbyProductid(int productid, string filter);
+        Task<List<GoodReceiptResponse>> getdetailsbyUserId(int userid, string filter);
+        Task<GoodReceiptResponse> disablebyid(int productid, string filter);
+        Task<GoodReceiptResponse> activebyid(int productid, string filter);
+        Task<List<GoodReceiptResponse>> getdisabledproductsbyuserid(int userid, string filter);
+        Task<List<GoodReceiptResponse>> getactiveproductsbyuserid(int userid, string filter);
+    }
+}
---
+++ ServerSide/ClassLibrary1/Interfaces/IGoodReceiptMapping.cs
@@ -0,0 +1,18 @@
+﻿using Shopping.Common.Requests;
+using Shopping.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.BLayer.Interfaces
+{
+    interface IGoodReceiptMapping
+    {
+        Task<GoodReceiptMappingResponse> savedetails(GoodReceiptMappingRequest data, string filter);
+        Task<GoodReceiptMappingResponse> updatedetails(GoodReceiptMappingRequest data, string filter);
+        // disable all mapping of user except grid which you pass
+        Task<GoodReceiptMappingResponse> disableGrMapping(int userid,int grid, string filter);
+        
+    }
+}
