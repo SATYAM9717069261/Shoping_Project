4cf51ba481639c6733c69209198bc4a3dc79ff07
SATYAM MANDAL <satyam.mandal.77@gmail.com>
2021-05-15 16:08:05 +0530

    Add Adapters

---
+++ ServerSide/ClassLibrary1/Adapters/GoodRceiptAdapter.cs
@@ -0,0 +1,58 @@
+﻿using Shopping.BLayer.Interfaces;
+using Shopping.Common.Requests;
+using Shopping.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.BLayer.Adapters
+{
+    public class GoodRceiptAdapter : IGoodReceipt
+    {
+        public Task<GoodReceiptResponse> activebyid(int productid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GoodReceiptResponse> disablebyid(int productid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<GoodReceiptResponse>> getactiveproductsbyuserid(int userid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GoodReceiptResponse> getdetailbyGrnumber(int grnum, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<GoodReceiptResponse>> getdetailsbyProductid(int productid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<GoodReceiptResponse>> getdetailsbyUserId(int userid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<GoodReceiptResponse>> getdisabledproductsbyuserid(int userid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GoodReceiptResponse> savedetails(GoodReceiptRequest data, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GoodReceiptResponse> updatedetails(GoodReceiptRequest data, string filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServerSide/ClassLibrary1/Adapters/GoodReceiptMappingAdapter.cs
@@ -0,0 +1,28 @@
+﻿using Shopping.BLayer.Interfaces;
+using Shopping.Common.Requests;
+using Shopping.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.BLayer.Adapters
+{
+    public class GoodReceiptMappingAdapter : IGoodReceiptMapping
+    {
+        public Task<GoodReceiptMappingResponse> disableGrMapping(int userid, int grid, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GoodReceiptMappingResponse> savedetails(GoodReceiptMappingRequest data, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GoodReceiptMappingResponse> updatedetails(GoodReceiptMappingRequest data, string filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
